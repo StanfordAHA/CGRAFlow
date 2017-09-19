4770a86fca3c29e6525bc554d2df97c346208e79
steveri <steveri@steveri.com>
2017-09-19 09:43:56 -0700

    Preparing for clean merge w/srtest

--- install.sh
+++ install.sh
@@ -86,23 +86,3 @@ pip install -e smt-pnr/package
 # need this for the new dot-compare test(s)
 # pip install pygtk
 sudo apt-get install python-gtk2
-
-# hello?  is this thing on???
-
-
-# VIRT=$PWD/CGRAFlowPy3Env
-# cd CGRAFlowPy3Env
-# 
-# wget http://pypi.python.org/packages/source/P/PyGTK/pygtk-2.24.0.tar.bz2
-# tar -xf pygtk-2.24.0.tar.bz2
-# cd pygtk-2.24.0
-# 
-# # export PKG_CONFIG_PATH=/home/PATH/TO/VIRT/lib/pkgconfig
-# export PKG_CONFIG_PATH=$VIRT/lib/pkgconfig
-# 
-# # ./configure --prefix=/home/PATH/TO/VIRT/
-# ./configure --prefix=$VIRT/
-# 
-# make 
-# make install
-# 
