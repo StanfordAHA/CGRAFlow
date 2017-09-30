bc109696aad2d36631f060d23d4053a6bee34ce2
steveri <steveri@steveri.com>
2017-09-29 21:55:07 -0700

    python 3.6 the hard way

--- install.sh
+++ install.sh
@@ -6,7 +6,22 @@ sudo apt-get update
 sudo apt-get install g++-4.9
 sudo apt-get install gcc-4.9
 
+python --version
+
+
 sudo apt-get install verilator luajit build-essential clang libedit-dev libpng-dev csh libgmp3-dev git cmake zlib1g zlib1g-dev graphviz-dev python3 swig2.0 libcln-dev imagemagick python-virtualenv libxml2-dev libxslt-dev python3-dev
+
+python --version
+
+
+
+sudo apt-get update
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6
+python --version
+
+
 
 if [[ -z "${TRAVIS_BUILD_DIR}" ]]; then
     # Halide_CoreIR/test/scripts/install_travis.sh is known to use this
