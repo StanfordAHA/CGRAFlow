947997ed7b2bcfa26b61506bb20b0daa2b19fb0d
rdaly525 <rdaly525@stanford.edu>
2017-10-03 10:01:05 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ export smt_git="https://github.com/makaimann/smt-switch"
 export halide_branch="dev"
 export coreir_branch="dev"
 export mapper_branch="dev"
-export cgra_branch="master"
+export cgra_branch="bsview"
 export pnr_branch="insane_onebit"
 export smt_branch="master"
 
