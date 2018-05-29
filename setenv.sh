e505fe71552b5199da02be5b4cdc04b76e8cca27
Lenny Truong <lenny@stanford.edu>
2018-05-29 15:29:13 -0700

    Update setenv.sh to use smt-pnr set env script

--- setenv.sh
+++ setenv.sh
@@ -5,19 +5,8 @@ export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_PATH/smt-switch
 export COREIR=$CGRAFLOW_PATH/coreir
 export LD_LIBRARY_PATH=$COREIR/lib:$LD_LIBRARY_PATH
 
-export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_PATH/smt_solvers/cvc4
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_PATH/smt_solvers/cvc4
+source $CGRAFLOW_PATH/smt-pnr/util/set_env_for_solvers.sh
 
-export PATH=$PATH:$CGRAFLOW_PATH/smt_solvers/monosat
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_PATH/smt_solvers/monosat
-
-export PATH=$PATH:$CGRAFLOW_PATH/smt_solvers/z3/bin/
-export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_PATH/smt_solvers/z3/bin/python/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_PATH/smt_solvers/z3/bin
-
-export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_PATH/smt_solvers/boolector
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_PATH/smt_solvers/boolector
-
-export CC=gcc-4.9 
-export CXX=g++-4.9 
+export CC=gcc-4.9
+export CXX=g++-4.9
 #export CGRA_SIZE=8x8
