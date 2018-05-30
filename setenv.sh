2b27abd16a5d85cc07f37ad5fb3e91b01e67b9e9
Lenny Truong <lenny@stanford.edu>
2018-05-30 09:58:28 -0700

    Set SMT_SOLVER_PATH variable

--- setenv.sh
+++ setenv.sh
@@ -5,6 +5,7 @@ export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_PATH/smt-switch
 export COREIR=$CGRAFLOW_PATH/coreir
 export LD_LIBRARY_PATH=$COREIR/lib:$LD_LIBRARY_PATH
 
+export SMT_SOLVER_PATH=smt-pnt/smt_solvers  # used by set_env_for_solvers.sh
 source $CGRAFLOW_PATH/smt-pnr/util/set_env_for_solvers.sh
 
 export CC=gcc-4.9
