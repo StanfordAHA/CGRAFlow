b8ac05c6dc28bff6202aec26df98aaf5f4b9c71a
Jeff Setter <setter@stanford.edu>
2019-02-22 19:31:37 -0800

    fix ordering of setenv

--- setenv.sh
+++ setenv.sh
@@ -3,11 +3,11 @@
 export PATH="$HOME/miniconda/bin:$PATH"
 export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_PATH/smt-switch
 
-export LD_LIBRARY_PATH=$COREIR/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/$CGRAFLOW_PATH/CGRAMapper/lib/"
-
 export COREIR=$CGRAFLOW_PATH/coreir
 export COREIRCONFIG="g++-4.9";
+
+export LD_LIBRARY_PATH=$COREIR/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CGRAFLOW_PATH/CGRAMapper/lib/"
 
 export LLVM_CONFIG=$CGRAFLOW_PATH/llvm/bin/llvm-config
 export CLANG=$CGRAFLOW_PATH/llvm/bin/clang
